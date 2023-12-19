df26db4bebcd1ced8da232e28cb13a999fc3cf16
rifqigenta <rifqigentabuana@students.amikom.ac.id>
2023-12-19 16:03:38 +0700

    update gambar

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const authRoutes = require('./routes/AuthRoutes');
-const productRoutes = require('./routes/ProductRoutes');
-const transactionRoutes = require('./routes/TransactionRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/AuthRoutes");
+const productRoutes = require("./routes/ProductRoutes");
+const transactionRoutes = require("./routes/TransactionRoutes");
 
 const app = express();
 const port = 4000;
@@ -11,10 +11,10 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/auth', authRoutes);
-app.use('/products', productRoutes);
-app.use('/transactions', transactionRoutes);
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use("/transactions", transactionRoutes);
+app.use("/uploads", express.static("uploads"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,20 +1,20 @@
 // models/UserModel.js
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'db',
+  host: "localhost",
+  user: "root",
+  password: "abc1234",
+  database: "db",
 });
 
 async function createUser(name, email, password, role_id) {
-    const [results] = await connection.execute('INSERT INTO users (name, email, password, role_id) VALUES (?, ?, ?, ?)', [name, email, password, role_id]);
-    return results.insertId;
-  }
+  const [results] = await connection.execute("INSERT INTO users (name, email, password, role_id) VALUES (?, ?, ?, ?)", [name, email, password, role_id]);
+  return results.insertId;
+}
 
 async function getUserByEmail(email) {
-  const [results] = await connection.execute('SELECT * FROM users WHERE email = ?', [email]);
+  const [results] = await connection.execute("SELECT * FROM users WHERE email = ?", [email]);
   return results[0];
 }
 
